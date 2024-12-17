e186e0e3e8b9146809ba5976dfd81d0e4bb64049
Kabanya <evonirazh@mail.ru>
2024-12-17 22:58:48 +0300

    Improved Class Car

--- miptC-sharpCourse/hw4/Car.cs
+++ miptC-sharpCourse/hw4/Car.cs
@@ -3,11 +3,10 @@ namespace miptC_sharpCourse.hw4
 {
     internal class Car
     {
-
-        public interface ICar
+        private interface ICar
         {
-            int Capacity { get; set; }
-            
+            int Capacity { get; } 
+            int CurrentPassengers { get; set; }
             void PersonCameIn(){}
             void PersonCameOut(){}
         }
@@ -22,19 +21,52 @@ namespace miptC_sharpCourse.hw4
 
         internal abstract class ACar : ICar, IMovement
         {
-            public int Capacity { get; set; }
+            private readonly int _capacity;
+            private int _currentPassengers;
+            protected ACar(int capacity, string name)
+            {
+                if (capacity < 0 || capacity > 7)
+                {
+                    Console.Error.WriteLine(
+                        $"STOP! In car must be between 0 and 7 passengers.{Environment.NewLine}" +
+                        $"However, actual capacity: {capacity} {Environment.NewLine}");
+                }
+                _capacity = capacity;
+                Name = name;
+                _currentPassengers = 0;
+            }
+
+            public string Name { get; }
+            public int Capacity => _capacity;
+            public int CurrentPassengers
+            {
+                get => _currentPassengers;
+                set => _currentPassengers = value;
+            }
 
             public void PersonGetIn()
             {
-                Console.WriteLine("Person came in car");
-                Capacity = Capacity + 1;
+                if (CurrentPassengers < Capacity)
+                {
+                    Console.WriteLine("Passenger came in car");
+                    CurrentPassengers++;
+                } else {Console.WriteLine("too much");}
             }
 
             public void PersonGetOut()
             {
-                Console.WriteLine("Person get out from car");
-                Capacity = Capacity - 1;
+                if (CurrentPassengers > 0)
+                {
+                    Console.WriteLine("Passenger get out from car");
+                    CurrentPassengers--;
+                } else {Console.WriteLine("There are no passengers to quitter the car");}
             }
+
+            public void HowManyPassengers()
+            {
+                Console.WriteLine($"There are {CurrentPassengers} passengers in the {Name}");
+            }
+            
             public abstract void MoveForward();
             public abstract void MoveBack();
             public abstract void TurnLeft();
@@ -43,92 +75,65 @@ namespace miptC_sharpCourse.hw4
 
         class ElectricMovement : IMovement
         {
-            public void MoveForward()
-            {
-                Console.WriteLine("Car moves forward in electric style");
-            }
-
-            public void MoveBack()
-            {
-                Console.WriteLine("Car moves back in electric style");
-            }
-
-            public void TurnLeft()
-            {
-                Console.WriteLine("Car turns left in electric style");
-            }
-
-            public void TurnRight()
-            {
-                Console.WriteLine("Car turns right in electric style");
-            }
-        }
-        
-        class VolvoMovement : IMovement
-        {
-            public void MoveForward()
-            {
-                Console.WriteLine("Volvo moves forward");
-            }
-
-            public void MoveBack()
-            {
-                Console.WriteLine("Volvo moves back");
-            }
-
-            public void TurnLeft()
-            {
-                Console.WriteLine("Volvo turns left");
-            }
-
-            public void TurnRight()
-            {
-                Console.WriteLine("Volvo turns right");
-            }
+            public void MoveForward() => Console.WriteLine("Electric car moves forward");
+            public void MoveBack() => Console.WriteLine("Electric car moves back");
+            public void TurnLeft() => Console.WriteLine("Electric car turns left");
+            public void TurnRight() => Console.WriteLine("Electric car turns right");
         }
 
-        public class Volvo : ACar
+        internal class VolvoMovement : IMovement
         {
-            // private readonly ElectricMovement _movement = new ElectricMovement();
+            public void MoveForward() =>  Console.WriteLine("Volvo moves forward");
+            public void MoveBack() =>  Console.WriteLine("Volvo moves back");
+            public void TurnLeft() => Console.WriteLine("Volvo moves left");
+            public void TurnRight() => Console.WriteLine("Volvo moves right");
+        }
 
+        public class Volvo(int capacity, string name) : ACar(capacity, name) //primary constructor 
+        {
             private IMovement _movement = new ElectricMovement();
-
             public IMovement Movement
             {
-                get { return _movement;}
-                set { _movement = value;}
+                get => _movement;
+                set => _movement = value;
             }
-
-
-            public override void MoveForward()
-            {
-                _movement.MoveForward();
-            }
-
-            public override void MoveBack()
-            {
-                _movement.MoveBack();
-            }
-
-            public override void TurnLeft()
-            {
-                _movement.TurnLeft();
-            }
-
-            public override void TurnRight()
-            {
-                _movement.TurnRight();
-            }
+            public override void MoveForward() => _movement.MoveForward();
+            public override void MoveBack() => _movement.MoveBack();
+            public override void TurnLeft() => _movement.TurnLeft();
+            public override void TurnRight() => _movement.TurnRight();
         }
-        
-        void CarExample()
+
+        public void CarExample()
         {
-            Volvo volvoV90Nilson = new Volvo();
+            Console.WriteLine($"{Environment.NewLine}!!! Car example code start.{Environment.NewLine}");
+            // Volvo volvoV90Nilson = new Volvo(capacity: 4, name:$"{nameof(volvoV90Nilson)}");
+            Volvo volvoV90Nilson = new Volvo(capacity: 8, name:$"Вольво V90 в модификации Нильсон");
+            volvoV90Nilson.CurrentPassengers = 1;
             volvoV90Nilson.TurnRight();
             VolvoMovement volvoMovement = new VolvoMovement();
             volvoV90Nilson.Movement = volvoMovement;
             volvoV90Nilson.Movement.TurnRight();
+            Console.WriteLine(volvoV90Nilson.CurrentPassengers);
+            Console.WriteLine(volvoV90Nilson.Name);
+            volvoV90Nilson.HowManyPassengers();
             volvoV90Nilson.PersonGetIn();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetIn();
+            volvoV90Nilson.CurrentPassengers = 8;
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetIn();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.CurrentPassengers = 4;
+            volvoV90Nilson.PersonGetOut();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetOut();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetOut();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetOut();
+            volvoV90Nilson.HowManyPassengers();
+            volvoV90Nilson.PersonGetOut();
+            Console.WriteLine($"{Environment.NewLine}!!! Car example code end.{Environment.NewLine}");
         }
     }
 }
--- miptC-sharpCourse/hw5/Insurance.cs
+++ miptC-sharpCourse/hw5/Insurance.cs
@@ -1,10 +1,14 @@
 using miptC_sharpCourse.hw4;
 /*
  ДЗ:
-1) Напишите собственную реализацию метода Equals для класса ElectricCar и для одного!) из классов, который наследуется от ElectricCar
-   (у кого то в дз была Тесла например). Используйте Equals для сравения двух машин, чтобы вызывались оба реализованных метода.
-2) Напишите класс CarInsurance, который будет по массе/типу двигателя/еще чему то (сами придумайте) будет выдавать сумму страховки 
-   за машину на 1 год по полученной машине (реализовать этот метод/методы статическим/статическими). 
+1) Напишите собственную реализацию метода Equals для класса ElectricCar и для одного!) 
+    из классов, который наследуется от ElectricCar
+   (у кого то в дз была Тесла например). Используйте Equals для сравения двух машин, 
+   чтобы вызывались оба реализованных метода.
+2) Напишите класс CarInsurance, который будет по массе/типу двигателя/еще чему то 
+    (сами придумайте) будет выдавать сумму страховки 
+    за машину на 1 год по полученной машине 
+    (реализовать этот метод/методы статическим/статическими). 
 3) * Сделайте CarInsurance синглтоном.
  */
 
@@ -12,15 +16,30 @@ namespace miptC_sharpCourse.hw5
 {
     internal class Insurance
     {
-        
+        class ElectricCar(int capacity, string name) : Car.Volvo(capacity, name)
+        {
+            public override bool Equals(object? obj)
+            {
+                bool isEqual = obj is ElectricCar;
+                return base.Equals(obj);
+            }
+        }
         static void Main()
         {
-            Car.Volvo electricCar = new Car.Volvo();
+            Car.Volvo volvoCar = new Car.Volvo(capacity: 4, name:$"{nameof(volvoCar)}");
+            Car.VolvoMovement volvoMovement = new Car.VolvoMovement();
+            volvoCar.Movement = volvoMovement;
+            ElectricCar electricCar1 = new ElectricCar(capacity: 4, name:$"{nameof(electricCar1)}");
+            ElectricCar electricCar2 = new ElectricCar(capacity: 4, name:$"{nameof(electricCar2)}");
 
-            electricCar.TurnLeft();
-            electricCar.TurnRight();
-            
-            Console.WriteLine("Program is finished");
+            volvoCar.TurnLeft();
+            electricCar2.TurnLeft();
+
+            var tempBool = electricCar1.Equals(electricCar2);
+            Console.WriteLine(tempBool);
+
+            Car car = new Car();
+            car.CarExample();
         }
     }
 }
